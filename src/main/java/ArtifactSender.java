774a21907c4c9148ba00c8d7079c57a7cd276c93
AlexWalker <alexjohn.walker@o2.com>
2017-11-27 11:03:53 +0000

    Added files and created separate methods for each function

--- src/main/java/ArtifactSender.java
+++ src/main/java/ArtifactSender.java
@@ -1,4 +1,5 @@
 
+
 import javax.mail.*;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
@@ -8,44 +9,65 @@ import java.util.Properties;
 import java.util.Scanner;
 
 public class ArtifactSender {
+    public ArtifactSender() throws FileNotFoundException {
+    }
+    String credentialsPath;//"/Users/alexwalker/DropboxApp/ArtifactEmailSender/emailCredentials"
+    String substitutionPath; //"/Users/alexwalker/DropboxApp/ArtifactEmailSender/substitutions"
+    String emailMessagePath; //"/Users/alexwalker/DropboxApp/ArtifactEmailSender/testDocumentation";
+    String emailDetailsPath; //"/Users/alexwalker/DropboxApp/ArtifactEmailSender/emailDetails";
+
+
+    String from;
 
     String recipient;
-    String credentialsPath;
-    String shareUrl;
+    String subject;
+    String carbonCopyRecipient;
 
-    String emailContents = "/Users/alexwalker/DropboxApp/EmailSender/testDocumentation";
-
-
-    String from = "Alexwalker97@outlook.com";
-    String to;
-
+    String completedEmail;
 
     //User Credentials
     String username;
     String password;
 
+    //get the credentials for the senders email.
     Properties credentialsProps = new Properties();
     InputStream inputProp;
 
+    //get the information about the email, e.g. Subject, Recipient, carbonCopyRecipient.
+    Properties infoProps = new Properties();
+    InputStream inputInfoProp;
+
+    //Email Server host and port
+    String host = "smtp-mail.outlook.com";
+    String port = "587";
+
     public static void main(String[] args) throws FileNotFoundException {
-        ArtifactSender artifactSender = new ArtifactSender();
-        if (args.length != 3) {
+
+        if (args.length != 4) {
             System.out.println("Missing Arguments: input path of file to upload, and path to key.");
             return;
         }
-//        artifactSender.go(args);
+        ArtifactSender artifactSender = new ArtifactSender();
+        System.out.println("Sending Email...");
 
+
+        artifactSender.setArguments(args);
+        artifactSender.readCredentials();
+        artifactSender.readEmailDetails();
         artifactSender.readEmail();
+        artifactSender.createEmail(args);
+
+        System.out.println("\n" + "Email Sent.");
     }
 
-    public void go(String[] args) {
+    public void setArguments(String[] args) {
+        credentialsPath = args[0];
+        emailMessagePath = args[1];
+        emailDetailsPath = args[2];
+        substitutionPath = args[3];
+    }
 
-        recipient = args[0];
-        credentialsPath = args[1];
-        shareUrl = args[2];
-
-        String host = "smtp-mail.outlook.com";
-        String port = "587";
+    public void createEmail(String[] args) {
 
         Properties props = new Properties();
         props.put("mail.smtp.auth", "true");
@@ -53,57 +75,77 @@ public class ArtifactSender {
         props.put("mail.smtp.host", host);
         props.put("mail.smtp.port", port);
 
+        Session session = Session.getInstance(props, new javax.mail.Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(username, password);
+            }
+        });
+
+        try {
+            //Crete a default MimeMessage object
+            Message message = new MimeMessage(session);
+            //set from: header field of the header
+            message.setFrom(new InternetAddress(from));
+            //set to: header field of the header
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(recipient));
+            //set cc: header field of the header
+            message.setRecipients(Message.RecipientType.CC, InternetAddress.parse(carbonCopyRecipient));
+            //set Subject: Header of the header
+            message.setSubject(subject);
+            //Set the message of the email
+            message.setText(completedEmail);
+            //Send email
+            Transport.send(message);
+        } catch (MessagingException ex) {
+            throw new RuntimeException(ex);
+        }
+
+    }
+    public void readEmail() throws FileNotFoundException {
+
+        String emailMessage = new Scanner(new File(emailMessagePath)).useDelimiter("/s").next();
+        ArrayList<String> substitutions = new ArrayList<>();
+
+        //Populating the ArrayList with strings from the substitutions file.
+        Scanner scanner = new Scanner(new File(substitutionPath)).useDelimiter("\\s");
+        while (scanner.hasNext()) {
+            substitutions.add(scanner.next());
+        }
+        scanner.close();
+
+        for (int i = 0; i < substitutions.size(); i++ ) {
+            emailMessage = emailMessage.replace("$$" + (i+1), substitutions.get(i));
+        }
+        completedEmail = emailMessage;
+        System.out.print(emailMessage);
+    }
+
+    public void readEmailDetails() {
+
+        try {
+            inputInfoProp = new FileInputStream(emailDetailsPath);
+            infoProps.load(inputInfoProp);
+
+            recipient = infoProps.getProperty("Recipient");
+            subject = infoProps.getProperty("Subject");
+            carbonCopyRecipient = infoProps.getProperty("CC");
+            from = infoProps.getProperty("From");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public void readCredentials() {
         try {
             inputProp = new FileInputStream(credentialsPath);
             credentialsProps.load(inputProp);
 
             username = credentialsProps.getProperty("username");
             password = credentialsProps.getProperty("password");
-
-            Session session = Session.getInstance(props, new javax.mail.Authenticator() {
-                @Override
-                protected PasswordAuthentication getPasswordAuthentication() {
-                    return new PasswordAuthentication(username, password);
-                }
-            });
-
-            try {
-                //Crete a default MimeMessage object
-                Message message = new MimeMessage(session);
-
-                //set from: header field of the header
-                message.setFrom(new InternetAddress(from));
-
-                //set to: header field of the header
-                message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(recipient));
-
-                //set Subject: Header of the header
-                message.setSubject("Test Subject");
-
-                //Set the message of the email
-                message.setText("hello, This is a test email ");
-
-                //Send email
-                Transport.send(message);
-            } catch (MessagingException ex) {
-                throw new RuntimeException(ex);
-            }
-
         } catch (IOException ex) {
             ex.printStackTrace();
         }
-
-
     }
-    public void readEmail() throws FileNotFoundException {
-        String emailString = new Scanner(new File(emailContents)).useDelimiter("/s").next();
 
-        String testVar = "this test var";
-
-        ArrayList<String> substitutions = new ArrayList<>();
-
-        for (int i = 0; i < substitutions.size(); i++ ) {
-            emailString = emailString.replace("$" + i + 1, substitutions.get(i));
-        }
-    }
 }
